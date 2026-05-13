0e46331d3d716c14f5dec2a9fded04343361c842
backend <backend.extreme@gmail.com>
2026-05-13 11:14:06 -0500

    feat:se modifica el archivo middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,14 @@ const defaultLocale = "en";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // 1. Evitar procesar archivos estáticos o internos
   if (
     pathname.startsWith("/_next") ||
     pathname.startsWith("/api") ||
-    pathname.includes(".") // Imágenes, favicons, etc.
+    pathname.includes(".") 
   ) {
     return NextResponse.next();
   }
 
-  // 2. Si el usuario intenta entrar con un prefijo manual (ej: /en/about), 
-  // lo redirigimos a la versión limpia (/about) pero guardando su preferencia en la cookie
   const foundLocale = locales.find(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
@@ -31,11 +28,8 @@ export function middleware(request: NextRequest) {
     response.cookies.set("NEXT_LOCALE", foundLocale, { path: "/" });
     return response;
   }
-
-  // 3. Obtener el idioma de la cookie o usar el defecto
   const locale = request.cookies.get("NEXT_LOCALE")?.value || defaultLocale;
 
-  // 4. REWRITE interno: el usuario ve /about, pero cargamos /[lang]/about
   return NextResponse.rewrite(
     new URL(`/${locale}${pathname}`, request.url)
   );
